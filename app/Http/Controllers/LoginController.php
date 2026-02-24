ee275c0d805194b42be055f8c0421663828860a8
gaonkars193@gmail.com <gaonkars193@gmail.com>
2026-02-24 18:55:28 +0530

    Register & Login With Middleware

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            return redirect()->route('post')->with('success', 'Login successful!');
+        } else {
+            return back()->withErrors(['email' => 'Invalid email or password.']);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Usercontroller;
+use App\Http\Middleware\ValidUser;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+Route::resource('registeruser', Usercontroller::class)->middleware(ValidUser::class);
+Route::get('/post', function () {
+    return view('post');
+})->name('post')->middleware(ValidUser::class);
